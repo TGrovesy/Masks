1e377cf9e137396eb4f8e383f7bd75f17393e2cb
Grovesy <99tgroves@gmail.com>
2020-05-18 16:20:22 +0100

    Initial commit

---
+++ Source/Masks/MasksGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MasksGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MASKS_API AMasksGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
