e1dbd0728b923580e4f1a0a21b416fc756b1e8c4
KrisTheCanadian <kristhecanadian101@gmail.com>
2020-05-22 19:34:14 -0400

    Basic Connection and Dedicated server working

--- Source/MasksServer.Target.cs
+++ Source/MasksServer.Target.cs
@@ -8,7 +8,6 @@ public class MasksServerTarget : TargetRules
 	public MasksServerTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Server;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		ExtraModuleNames.AddRange( new string[] { "Masks" } );
+		ExtraModuleNames.Add("Masks");
 	}
 }
